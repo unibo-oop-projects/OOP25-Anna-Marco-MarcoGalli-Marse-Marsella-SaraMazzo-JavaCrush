32dd73639a2c6006c9eca7bc523c8f332ddba8ca
marsella <gianluca.marsella@studio.unibo.it>
2026-03-26 12:01:39 +0100

    defined basic structure

---
+++ src/main/java/it/unibo/javacrush/common/CellType.java
@@ -0,0 +1,10 @@
+package it.unibo.javacrush.common;
+
+public enum CellType {
+    COFFEE_BEAN,
+    SUGAR,
+    CUP,
+    MILK,
+    MOKA,
+    SPOON;
+}
---
+++ src/main/java/it/unibo/javacrush/common/Position.java
@@ -0,0 +1,5 @@
+package it.unibo.javacrush.common;
+
+public record Position() {
+
+}
