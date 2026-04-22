a26cf1c19bc673f5f8d9ad31ffc187d05211a5f3
marsella <gianluca.marsella@studio.unibo.it>
2026-04-22 20:24:26 +0200

    Implement Junit test for GravityEngineTest

--- src/main/java/it/unibo/javacrush/model/impl/DownwardGravity.java
+++ src/main/java/it/unibo/javacrush/model/impl/DownwardGravity.java
@@ -3,12 +3,12 @@ package it.unibo.javacrush.model.impl;
 import it.unibo.javacrush.model.api.Board;
 import it.unibo.javacrush.model.api.GravityEngine;
 
-public class GravityEngineImpl implements GravityEngine {
+public class DownwardGravity implements GravityEngine {
 
     @Override
     public void applyGravity(Board board) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'applyGravity'");
+        
+        
     }
 
 }
