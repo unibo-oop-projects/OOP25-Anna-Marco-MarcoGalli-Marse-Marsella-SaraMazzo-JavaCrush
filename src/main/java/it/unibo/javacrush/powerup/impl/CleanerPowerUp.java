9177b109ce9ed191927dc3533dff344d3785d13f
Marco Galli <marco.galli14@studio.unibo.it>
2026-05-08 02:11:22 +0200

    created PowerUp interface and relative implementation classes

---
+++ src/main/java/it/unibo/javacrush/powerup/impl/CleanerPowerUp.java
@@ -0,0 +1,15 @@
+package it.unibo.javacrush.powerup.impl;
+
+import it.unibo.javacrush.common.Position;
+import it.unibo.javacrush.model.api.Board;
+import it.unibo.javacrush.powerup.api.PowerUp;
+
+public class CleanerPowerUp implements PowerUp{
+
+    @Override
+    public Boolean applyPowerUp(Board board, Position pos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'applyPowerUp'");
+    }
+    
+}
