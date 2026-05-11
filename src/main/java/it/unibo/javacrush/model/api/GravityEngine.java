f422125c5ac6d425095d1107a6be560d91313214
marsella <gianluca.marsella@studio.unibo.it>
2026-05-11 11:30:30 +0200

    add static method to get random gravity engine instance

--- src/main/java/it/unibo/javacrush/model/api/GravityEngine.java
+++ src/main/java/it/unibo/javacrush/model/api/GravityEngine.java
@@ -1,6 +1,14 @@
 package it.unibo.javacrush.model.api;
 
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+
 import it.unibo.javacrush.common.Direction;
+import it.unibo.javacrush.model.impl.gravity.DownwardGravity;
+import it.unibo.javacrush.model.impl.gravity.LeftwardGravity;
+import it.unibo.javacrush.model.impl.gravity.RightwardGravity;
+import it.unibo.javacrush.model.impl.gravity.UpwardGravity;
 
 /**
  * Interface representing the gravity engine in the game.
@@ -21,4 +29,19 @@ public interface GravityEngine {
      * @return the current gravity direction
      */
     Direction getDirection();
+
+    /**
+     * get a random gravity engine instance.
+     * @return a random gravity engine instance
+     */
+    static GravityEngine getRandom() {
+        List<Supplier<GravityEngine>> strategies = List.of(
+            DownwardGravity::new,
+            UpwardGravity::new,
+            LeftwardGravity::new,
+            RightwardGravity::new
+        );
+        int randomIndex = new Random().nextInt(strategies.size());
+        return strategies.get(randomIndex).get();
+    }
 }
