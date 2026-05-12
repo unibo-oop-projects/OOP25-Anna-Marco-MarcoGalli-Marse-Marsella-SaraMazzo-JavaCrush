70077c41139497fe186e20ea525e2850e5400e27
marsella <gianluca.marsella@studio.unibo.it>
2026-05-12 17:47:25 +0200

    implement LevelManagerImpl methods for level setup and star management; remove unimplemented method stubs

--- src/main/java/it/unibo/javacrush/model/impl/LevelManagerImpl.java
+++ src/main/java/it/unibo/javacrush/model/impl/LevelManagerImpl.java
@@ -1,26 +1,31 @@
 package it.unibo.javacrush.model.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import it.unibo.javacrush.model.api.LevelConfig;
 import it.unibo.javacrush.model.api.LevelManager;
 
 public class LevelManagerImpl implements LevelManager {
 
+    private final Map<Integer, Integer> starsRecords = new HashMap<>();
+
     @Override
-    public LevelConfig getLevelSetup(int level) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getLevelSetup'");
+    public LevelConfig getLevelSetup(int level) {    
+        return LevelFactory.createLevel(level);
     }
 
     @Override
     public void updateStars(int level, int stars) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateStars'");
+        int currentBest = starsRecords.getOrDefault(level, 0);
+        if (stars > currentBest) {
+            starsRecords.put(level, stars);
+        }
     }
 
     @Override
     public int getStarsForLevel(int level) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getStarsForLevel'");
+        return starsRecords.getOrDefault(level, 0);
     }
 
 }
