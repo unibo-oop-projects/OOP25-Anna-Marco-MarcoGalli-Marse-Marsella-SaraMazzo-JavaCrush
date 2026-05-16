89ce2b61ae19200bb582909d27055f35ac2c1402
Anna <anna.pollini2@studio.unibo.it>
2026-05-16 19:24:10 +0200

    Partially implementation of GameController

--- src/main/java/it/unibo/javacrush/controller/api/GameController.java
+++ src/main/java/it/unibo/javacrush/controller/api/GameController.java
@@ -1,5 +1,7 @@
 package it.unibo.javacrush.controller.api;
 
+import it.unibo.javacrush.common.CellType;
+import it.unibo.javacrush.common.GameState;
 import it.unibo.javacrush.common.Position;
 
 /**
@@ -9,27 +11,41 @@ import it.unibo.javacrush.common.Position;
 public interface GameController {
 
     /**
-     * Handle the click on the board, it will select the cell and then deselect it if it's already selected.
+     * Handle the click on the board, it will select the cell and then deselect 
+     * it if it's already selected.
      *
      * @param p the position of the cell clicked
-     *
-     * @return true if we can select the cell, false otherwise
+     * @return true if the cells are swapped, false otherwise
      */
     boolean hit(Position pos);
-
-    /**
-     * Swap two cells.
-     *
-     * @param p1 the first selected cell
-     * @param p2 the second selected cell
-     */
-    void swap(Position p1, Position p2);
 
     /**
      * Start the game loop, it will be called when the level is loaded and will
      * handle the game logic until the level is completed or the player quits.
      */
     void gameLoop();
+
+    /**
+     * Update the game state.
+     *
+     * @return the updated game state
+     */
+    GameState updateGameState();
+
+    /**
+     * Gett the CellType for a specific cell in the board.
+     * 
+     * @param pos position of the cell
+     * @return type of the cell
+     */
+    CellType getCellTypeAtPos(Position pos);
+
+    /**
+     * Get the remaining moves.
+     * 
+     * @return the numeber of remaining moves
+     */
+    int getMovesLeft();
 
     /**
      * Quit the level and go back to the main menu.
--- src/main/java/it/unibo/javacrush/controller/impl/GameControllerImpl.java
+++ src/main/java/it/unibo/javacrush/controller/impl/GameControllerImpl.java
@@ -1,115 +1,134 @@
 package it.unibo.javacrush.controller.impl;
 
 import java.util.Map;
+import java.util.Set;
 
 import it.unibo.javacrush.common.CellType;
 import it.unibo.javacrush.common.Position;
 import it.unibo.javacrush.common.GameState;
 import it.unibo.javacrush.controller.api.GameController;
 import it.unibo.javacrush.model.api.Board;
+import it.unibo.javacrush.model.api.GameMatchContext;
 import it.unibo.javacrush.model.api.GravityEngine;
-import it.unibo.javacrush.model.api.LevelConfig;
+import it.unibo.javacrush.model.api.Match;
 import it.unibo.javacrush.model.api.MatchManager;
 import it.unibo.javacrush.model.api.MoveEngine;
 import it.unibo.javacrush.model.api.PhysicsHandler;
+import it.unibo.javacrush.model.api.Session;
 import it.unibo.javacrush.model.api.StallEngine;
-import it.unibo.javacrush.model.impl.BoardImpl;
-import it.unibo.javacrush.model.impl.MatchManagerImpl;
-import it.unibo.javacrush.model.impl.MoveEngineImpl;
-import it.unibo.javacrush.model.impl.PhysicsHandlerImpl;
-import it.unibo.javacrush.model.impl.StallEngineImpl;
 
 /**
  * Implementation of the {@link GameController} interface.
  */
 public class GameControllerImpl implements GameController{
 
-    private final int rows;
-    private final int cols;
-    private final int moves;
+    // TO BE CHANGED BASED ON GAMEMATCHCONTEXT
+    //private final int rows;
+    //private final int cols;
+    //private final int moves;
     private final Map<CellType, Integer> goals;
     private final GravityEngine gravity;
     private final Board board;
     private final PhysicsHandler physics;
     private final StallEngine stallEngine;
-    //private final Session session;
+    private final Session session;
     private final MoveEngine moveEngine;
     private final MatchManager matchManager;
+
+    private Position lastClickedPosition = null;
+    private Set<Match> matches = Set.of();
     
 
-    public GameControllerImpl(final LevelConfig config) {
-        this.rows = config.rows();
-        this.cols = config.cols();
-        this.moves = config.moves();
-        this.goals = config.goals();
-        this.gravity = config.gravity();
-
-        // Create Board
-        this.board = new BoardImpl(rows, cols);
-
-        //this.session = new SessionImpl
-
-        this.matchManager = new MatchManagerImpl();
-
-        this.moveEngine = new MoveEngineImpl();
-
-        this.stallEngine = new StallEngineImpl();
-
-        this.physics = new PhysicsHandlerImpl(this.gravity, this.stallEngine);
+    public GameControllerImpl(final GameMatchContext gameContext) {
+        //this.rows = gameContext.getLevelConfig().rows();
+        //this.cols = gameContext.getLevelConfig().cols();
+        this.board = gameContext.getBoard();
+        //this.moves = gameContext.getLevelConfig().moves();
+        this.goals = gameContext.getLevelConfig().goals();
+        this.session = gameContext.getSession();
+        this.gravity = gameContext.getLevelConfig().gravity();
+        this.physics = gameContext.getPhysicsHandler();
+        this.stallEngine = gameContext.getStallEngine();
+        this.moveEngine = gameContext.getMoveEngine();
+        this.matchManager = gameContext.getMatchManager();
     }
 
-    /**
-     * Handle the click on the board, it will select the cell and then deselect it if it's already selected.
-     *
-     * @param p the position of the cell clicked
-     *
-     * @return true if we can select the cell, false otherwise
-     */
+
+    @Override
     public boolean hit(final Position pos) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'hit'");
+        if (this.lastClickedPosition.equals(null)) {
+            // If the cell is null it can't be swapped
+            this.lastClickedPosition = pos;
+            return false;
+        } else {
+            boolean canSwap = this.swap(this.lastClickedPosition, pos);
+            this.lastClickedPosition = null;
+            if (canSwap) {
+                this.session.decreaseMoves();
+                this.matches = this.matchManager.findAllMatches(this.board);
+            }
+            return canSwap;
+        }
     }
 
+
+    @Override
+    public void gameLoop() {
+        // Update and remove matches from the board
+        this.matches.stream()
+            .forEach(match -> {
+                this.session.updateGoals(match.getType(), match.getSize());
+                this.matchManager.removeMatch(this.board, match);
+        });
+        this.matches = null;
+
+        // Update view
+
+        // VA STACCATO
+        // Apply gravity -> ciclata e per ogni ciclo update view
+
+        // Check if the game is stalled, if so apply stall engine
+
+        // Update view
+    }
+
+
+    @Override
+    public GameState updateGameState() {
+        return this.session.getGameStatus();
+    }
+
+
+    @Override
+    public CellType getCellTypeAtPos(final Position pos) {
+        return this.board.getCellAt(pos).get().getType();
+    }
+
+    @Override
+    public int getMovesLeft() {
+        return this.session.getMovesLeft();
+    }
+
+    @Override
+    public void quitLevel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'quitLevel'");
+    }
 
     /**
      * Swap two cells.
      *
      * @param p1 the first selected cell
      * @param p2 the second selected cell
+     * @return true if the cells are swapped, false otherwise
      */
-    public void swap(final Position p1, final Position p2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'swap'");
-    }
-
-
-    /**
-     * Select a power-up and activate it.
-     *
-     * @param powerUpNumber the number of the power-up selected
-     */
-    void selectPowerUp(final int powerUpNumber) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'selectPowerUp'");
-    }
-
-
-    /**
-     * Start the game loop, it will be called when the level is loaded and will
-     * handle the game logic until the level is completed or the player quits.
-     */
-    public void gameLoop() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'gameLoop'");
-    }
-
-
-    /**
-     * Quit the level and go back to the main menu.
-     */
-    public void quitLevel() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'quitLevel'");
+    private boolean swap(final Position p1, final Position p2) {
+        if (moveEngine.canSwap(this.board, p1, p2)) {
+            this.board.swapCells(p1, p2);
+            this.session.decreaseMoves();
+            return true;
+        }
+        return false;
     }
 
 }
